77f0014dd3c25fa72e2926d42aaab2f0e51fb084
Tony Habash <ttonyh@gmail.com>
2018-07-05 12:24:34 -0700

    Updated pronto

--- vue-pronto-test-1.js
+++ vue-pronto-test-1.js
@@ -19,16 +19,12 @@ const renderer = new Pronto( prontoOpts );
 
 
 const data = {
-    bar: true,
-    fakehtml: '<p class="red">FAKEHTML</p>'
+
 };
 
-const templateLiteral = `<!DOCTYPE html>\n<html lang="en">\n<head>\n<title>{{title}}</title>\n<style>{{css}}</style>\n</head>\n<body>\n<h1>FOOOOO</h1>\n<!--vue-ssr-outlet-->\n</body>\n</html>`;
 
 const vueOptions = {
-    title: "Test",
-    // template: templateLiteral
-    template: ''
+
 };
 
 const file = './src/index.vue';
