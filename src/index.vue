f28ebfe8f4fcccb90d2401a2ae3d4a13961e141d
Tony Habash <ttonyh@gmail.com>
2018-07-09 19:24:35 -0700

    Added meta attribute

--- src/index.vue
+++ src/index.vue
@@ -32,6 +32,17 @@
                 showHidden: false
             };
         },
+        
+        
+        meta: {
+            title: 'THIS SHOULD BE COMING FROM THE FUNNEL....',
+            meta: {
+                description: { name: 'description', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis eros risus, eu scelerisque augue molestie quis. Aenean id lectus arcu. Aenean et tristique sem. Suspendisse in pellentesque ligula. Morbi venenatis ex eros, sit amet ultricies odio fringilla et. Suspendisse scelerisque mollis posuere. Aenean odio justo, dictum in ultrices non, dapibus dignissim dui. Aliquam porttitor vitae ligula sit amet venenatis. Nullam ex libero, placerat sed metus non, mollis vulputate felis. Maecenas massa orci, placerat in hendrerit a, sollicitudin in felis. Aliquam lobortis orci ut maximus sagittis. Nulla a arcu sodales, convallis ex at, venenatis dui. Morbi non nisi interdum, venenatis metus ut, lobortis eros. Mauris sed pretium nunc. Nunc ut tristique sem.' },
+                keywords: { name: 'keywords', content: 'foo bar blah blah blah' }
+            }
+        },
+        
+        
         mounted() {
             console.info( 'This has been mounted...' );
         },
