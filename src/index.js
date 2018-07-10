2cc87851946fa56b87513f61d37220a202e68ee7
Tony Habash <ttonyh@gmail.com>
2018-07-10 14:31:59 -0700

    Updated to routes obj

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import PageHome from './views/home';
-import PagePlans from './views/plans';
-import PageDevices from './views/devices';
+import viewHome from './views/home';
+import viewPlans from './views/plans';
+import viewDevices from './views/devices';
 
-export default {
-    PageHome,
-    PagePlans,
-    PageDevices
-};
+
+export default [
+    {
+        path: '/offer/:funnelId/',
+        name: 'home',
+        component: viewHome
+    },    
+    {
+        path: '/offer/:funnelId/plans',
+        name: 'plans',
+        component: viewPlans
+    },
+    {
+        path: 'offer/:funnelId/devices',
+        name: 'devices',
+        component: viewDevices
+    }
+];
+
