c226f58519e1ff44255ca71d441bddc50bae7bda
Tony Habash <ttonyh@gmail.com>
2018-07-11 18:38:05 -0700

    Changed

--- src/index.js
+++ src/index.js
@@ -7,26 +7,18 @@ import viewDevices from './views/devices';
 export default [
     {
         path: '/offer/:funnelId',
-        name: 'home',
-        redirect: { name: 'offer' },
-        component: viewIndex,
-        children: [
-            {
-                path: '/offer/:funnelId',
-                name: 'offer',
-                component: viewHome
-            },
-            {
-                path: '/offer/:funnelId/plans',
-                name: 'plans',
-                component: viewPlans
-            },
-            {
-                path: '/offer/:funnelId/devices',
-                name: 'devices',
-                component: viewDevices
-            }
-        ]
+        name: 'offer',
+        component: viewHome
+    },
+    {
+        path: '/offer/:funnelId/plans',
+        name: 'plans',
+        component: viewPlans
+    },
+    {
+        path: '/offer/:funnelId/devices',
+        name: 'devices',
+        component: viewDevices
     }
 ];
 
